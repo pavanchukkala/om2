3cc8dcbfbd8e00dae6c7fa6c4e6325ba4ee2b1f8
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2024-12-24 22:52:08 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,30 +1,10 @@
-// models/User.js
-// User database model
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user',
-  },
-}, { timestamps: true });
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },  // hashed password
+    salt: { type: String, required: true },      // salt for password
+});
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
