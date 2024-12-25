3bdfaf3f62a351d629ddc0d7c94a9c4974d758f4
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2024-12-25 11:16:39 +0530

    Update contentRoutes.js

--- server/routes/contentRoutes.js
+++ server/routes/contentRoutes.js
@@ -1,11 +1,12 @@
-// routes/contentRoutes.js
-// Content-related API routes
+// server/routes/contentRoutes.js
 
 const express = require('express');
 const { getContent, createContent } = require('../controllers/contentController');
 const authMiddleware = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
+// Define routes
 router.get('/', getContent);
 router.post('/', authMiddleware, createContent);
 
