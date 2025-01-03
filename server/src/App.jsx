56dcde91b3be19c8e6d72bdee387db89d0fe206d
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-01-03 15:18:16 +0530

    Update App.jsx

--- server/src/App.jsx
+++ server/src/App.jsx
@@ -1,25 +1,24 @@
-// App.jsx
-// Main app entry component
-
-import React from 'react';
-import { Provider } from 'react-redux';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './store/store';
-import Home from './pages/Home';
-import About from './pages/About';
 import ThemeSwitcher from './components/ThemeSwitcher';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const About = React.lazy(() => import('./pages/About'));
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <div>
-          <ThemeSwitcher />
+        <ThemeSwitcher />
+        <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="*" element={<div>404 - Page Not Found</div>} />
           </Routes>
-        </div>
+        </Suspense>
       </Router>
     </Provider>
   );
